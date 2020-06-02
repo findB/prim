455c27a05481aecbac57532fb0edd5bfb708d1e4
gj <profmokytojas@gmail.com>
2020-06-02 16:38:37 +0300

    Initial commit

---
+++ back/src/main/java/it/akademija/prim/PrimApplication.java
@@ -0,0 +1,13 @@
+package it.akademija.prim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/it/akademija/prim/PrimApplicationTests.java
@@ -0,0 +1,13 @@
+package it.akademija.prim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
